6879d585c9b3632e51b158305bc7b4ebc2a0301d
BrainStarr <brian.r.starr@gmail.com>
2014-10-31 15:36:37 -0700

    completed

--- Drawing a House/HouseView.m
+++ Drawing a House/HouseView.m
@@ -10,12 +10,26 @@
 
 @implementation HouseView
 
-/*
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     // Drawing code
+    
+    UIBezierPath *houseOutline = [UIBezierPath bezierPath];
+    houseOutline.lineWidth = 2.0;
+    
+    [houseOutline moveToPoint:CGPointMake(1/5.0 * self.bounds.size.width, 4/5.0 * self.bounds.size.height)];
+    [houseOutline addLineToPoint:CGPointMake(1/5.0 * self.bounds.size.width, 3/5.0 * self.bounds.size.height)];
+    [houseOutline addLineToPoint:CGPointMake(2.0/5.0 * self.bounds.size.width, 2.25/5.0 * self.bounds.size.height)];
+    [houseOutline addLineToPoint:CGPointMake(3/5.0 * self.bounds.size.width, 3/5.0 * self.bounds.size.height)];
+    [houseOutline addLineToPoint:CGPointMake(3/5.0 * self.bounds.size.width, 4/5.0 * self.bounds.size.height)];
+    //[houseOutline addLineToPoint:CGPointMake(1/5.0 * self.bounds.size.width, 3/5.0 * self.bounds.size.height)];
+    
+    [houseOutline closePath];
+    [houseOutline stroke];
+    
 }
-*/
+
 
 @end
