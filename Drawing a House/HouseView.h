388ad0b447b9aa426c026f30929732a812a74b1f
BrainStarr <brian.r.starr@gmail.com>
2014-10-31 15:00:12 -0700

    initial setup

---
+++ Drawing a House/HouseView.h
@@ -0,0 +1,13 @@
+//
+//  HouseView.h
+//  Drawing a House
+//
+//  Created by Brian Starr on 10/31/14.
+//  Copyright (c) 2014 Brian Starr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HouseView : UIView
+
+@end
---
+++ Drawing a House/HouseView.m
@@ -0,0 +1,21 @@
+//
+//  HouseView.m
+//  Drawing a House
+//
+//  Created by Brian Starr on 10/31/14.
+//  Copyright (c) 2014 Brian Starr. All rights reserved.
+//
+
+#import "HouseView.h"
+
+@implementation HouseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
